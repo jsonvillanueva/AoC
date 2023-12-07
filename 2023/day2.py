bd1bc611a29f105af2a40c9acaaee19d82e3fc18
Jason G. Villanueva <a@jsonvillanueva.com>
2023-12-06 18:19:29 -0800

    Apply black formatting

--- 2023/day2.py
+++ 2023/day2.py
@@ -13,6 +13,7 @@ def parse_line(line: str):
             colors[color] = max(colors[color], int(amount))
         return int(id_), colors
 
+
 def part_one(line: str) -> int:
     max_colors = Counter(red=12, green=13, blue=14)
     id_, colors = parse_line(line)
@@ -22,6 +23,7 @@ def part_one(line: str) -> int:
 def part_two(line: str) -> int:
     _, colors = parse_line(line)
     return math.prod(colors.values())
+
 
 if __name__ == "__main__":
     print(sum(parse_file_line_by_line(2, 1, part_one)))
